95b73ae2574098ab26c59350c6a7bfce67a6dc82
Hayden Neumeyer <hadn6969@gmail.com>
2018-02-07 21:56:03 -0800

    move trans logo

--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,7 @@
 
 <div class="container">
 	<div class="footer" style="clear: both; margin-top: 30px; height:60px; text-align: center; font-size:14px; color: rgb(150, 150, 150);">
-    	<span class="ion-transgender"> &copy; 2018 Hadn69. Site originally designed by Nijikokun and Aesen for the <a href="http://www.spongepowered.org/" target="_blank">SpongePowered</a> project and modified by <a href="https://github.com/TehNut/tehnut.info">TehNut</a>. I take no ownership of their work.</span><br>
+    	<span> &copy; 2018 <span class="ion-transgender"></span>Hadn69. Site originally designed by Nijikokun and Aesen for the <a href="http://www.spongepowered.org/" target="_blank">SpongePowered</a> project and modified by <a href="https://github.com/TehNut/tehnut.info">TehNut</a>. I take no ownership of their work.</span><br>
       	<span>Header image is fanart of Portal turrets</span>
 	</div>
 </div>
