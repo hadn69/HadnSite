35fd84e6994130786673a8f0b4254a51b60600b7
Hayden Neumeyer <hadn6969@gmail.com>
2018-02-07 21:36:10 -0800

    fixed shit

--- include/header.php
+++ include/header.php
@@ -5,9 +5,8 @@
           		<a href="../"><h1>Hayden Neumeyer</h1></a>
           		<h2>Owner and System Administrator of BlackSun.Network</h2><br/>
                 	Depressed Dick Sucker<br/>
-            		<a href="https://blacksun.network" class="btn btn-large btn-full"><span class="ios-construct">  </span>Black Sun Network</a>
-            		<a href="mailto:hayden@daemon.group" target="_blank" class="btn btn-large btn-empty"><span class="ion-android-mail">  </span>Contact Me</a>
-            		<a href="https://twitch.tv/Hadn69/" target="_blank" class="btn btn-large btn-full"><span class="logo-twitch">  </span>Twitch</a>
+            		<a href="https://blacksun.network" class="btn btn-large btn-full"><span class="ion-settings">  </span>Black Sun Network</a>
+            		<a href="mailto:hayden@daemon.group" target="_blank" class="btn btn-large btn-full"><span class="ion-android-mail">  </span>Contact Me</a>
         	</div>
     	</div>
 	</div>
