648ab8387a38853df66d4736206b336bf91e0eab
Hayden Neumeyer <hadn6969@gmail.com>
2018-02-07 21:26:55 -0800

    fixed icons

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <link rel="shortcut icon" href="/images/favicon.png">
   <script src="anal.js"></script>
   <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-  <link href='http://code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css' rel='stylesheet' type='text/css' />
+  <link href='css/ionicons.css' rel='stylesheet' type='text/css' />
   <link href='css/main.css' rel='stylesheet' type='text/css' />
 </head>
 <body>
@@ -15,9 +15,9 @@
   <div class="container">
     <div class="box">
       <div class="inner">
-        <a href="https://twitter.com/OfficialTehNut" target="_blank"><span class="icon ion-social-twitter"></span>
-        <h2>Twitter</h2></a>
-	      <p>For when I occasionally tweet stuff.</p>
+        <a href="hayden_resume.pdf" target="_blank"><span class="icon ion-social-twitter"></span>
+        <h2>My Resume</h2></a>
+	      <p>In case you want to know more about me.</p>
       </div>
     </div>
 
@@ -25,7 +25,7 @@
       <div class="inner">
         <a href="https://github.com/Hadn69/" target="_blank"><span class="icon ion-social-github"></span>
         <h2>GitHub</h2></a>
-        <p>Most things I work on are open source. Here it is.</p>
+        <p>Most of the things I work on are open source.</p>
       </div>
     </div>
 
