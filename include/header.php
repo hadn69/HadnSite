0ec1c3bc2832931f8a8282e571fbf8d82a5e4124
Hayden Neumeyer <hadn6969@gmail.com>
2018-06-25 21:22:50 -0700

    |

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
         	<div class="hero">
           		<a href="../"><h1>Hayden Neumeyer</h1></a>
           		<h2>Owner and System Administrator of BlackSun.Network</h2><br/>
-                	<div class="rainbow">Transgender Moon Princess</div> | AKA: Hadn69<br/>
+                	<div class="rainbow">Transgender Moon Princess</div> AKA: Hadn69<br/>
             		<a href="https://blacksun.network" class="btn btn-large btn-full"><span class="ion-settings">  </span>Black Sun Network</a>
             		<a href="mailto:moon@daemon.group" target="_blank" class="btn btn-large btn-full"><span class="ion-android-mail">  </span>Contact Me</a>
 					<a href="/web/pgp.txt" target="_blank" class="btn btn-large btn-full"><span class="ion-android-mail">  </span>PGP Key</a>
