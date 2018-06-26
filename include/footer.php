49715b27f775a44a98b5a0f9b70e6f23a3b66df2
Hayden Neumeyer <hadn6969@gmail.com>
2018-06-25 21:33:46 -0700

    <!––

--- include/footer.php
+++ include/footer.php
@@ -3,6 +3,6 @@
 <div class="container">
 	<div class="footer" style="clear: both; margin-top: 30px; height:60px; text-align: center; font-size:14px; color: rgb(150, 150, 150);">
     	<span> &copy; 2018 <span class="ion-transgender"></span>Hadn69. Site originally designed by Nijikokun and Aesen for the <a href="http://www.spongepowered.org/" target="_blank">SpongePowered</a> project and modified by <a href="https://github.com/TehNut/tehnut.info">TehNut</a>. I take no ownership of their work.</span><br>
-      	<span>Header image is from <a href="https://www.warframe.com/thesacrifice">Waframe</a> and &copy; Digital Extremes</span>
+			<!–– <span>Header image is from <a href="https://www.warframe.com/thesacrifice">Waframe</a> and &copy; Digital Extremes</span >-->
 	</div>
 </div>
